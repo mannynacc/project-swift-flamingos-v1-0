4ec1694ed6fb630e8589b8dc763b6778db2aa764
Nacho Cota <manuelcota01@outlook.com>
2022-06-24 06:03:25 +0000

    Trying to append to HTML

--- app/static/js/get_timeline_posts.js
+++ app/static/js/get_timeline_posts.js
@@ -2,27 +2,30 @@ async function getPosts () {
     res = await fetch('http://mlhportofolio.duckdns.org:5000/api/timeline_post', {
     method: 'GET'
     });
-    data = await res.json();
-    return data;
+	data = await res.json();
+	console.log(data.timeline_posts);
+	return data;
 }
 
 function printPosts(posts) {
-    for(post in posts)
+	console.log("POSTS: ", posts);
+	
+	const timeline = document.getElementById('timeline');
+	
+	for(post in posts)
     {
         console.log(post);
-        $('#timeline').append(
+        timeline.innerHTML +=
         `<div class="post" id="post-${post.id}>
             <span class="name>${post.name}</span>
             <span class="name>${post.email}</span>
             <span class="name>${post.created_at}</span>
 
             <p>${post.content}</p>
-        </div>`
-        );
+        </div>`;
     }
 }
 
-posts = getPosts()
-        .then(data => console.log(data))
-        .then(posts => printPosts(posts));
+posts = getPosts().then(data => console.log(data));
 
+printPosts(posts.timeline_posts);
