8d47e6dd60c4350b7ab3dd4e91b539e25b145a2c
Nacho Cota <manuelcota01@outlook.com>
2022-06-24 17:36:50 +0000

    Fixed error preventing from posting new content

--- app/static/js/post_timeline.js
+++ app/static/js/post_timeline.js
@@ -5,11 +5,38 @@ form.addEventListener('submit', function(e) {
     e.preventDefault();
     // Create payload as new FormData object:
     const payload = new FormData(form);
+
     // Post the payload using Fetch:
     fetch('http://mlhportofolio.duckdns.org:5000/api/timeline_post', {
     method: 'POST',
     body: payload,
     })
-    .then(res => res.json())
+    .then(res => res.text())
     .then(data => console.log(data))
 })
+
+async function getPosts () {
+    res = await fetch('http://mlhportofolio.duckdns.org:5000/api/timeline_post', {
+    method: 'GET'
+    });
+	data = await res.json();
+    posts = data.timeline_posts;
+
+    const timeline = document.getElementById('timeline');
+	
+	for(i in posts)
+    {
+        post = posts[i];
+        console.log(post);
+        timeline.innerHTML +=
+        `<div class="post" id="post-${post.id}>
+            <span class="name>${post.name}</span>
+            <span class="name>${post.email}</span>
+            <span class="name>${post.created_at}</span>
+
+            <p>${post.content}</p>
+        </div>`;
+    }
+}
+
+posts = getPosts();
