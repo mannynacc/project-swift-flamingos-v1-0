65bd609cca13c2de96e1a9bf36008ac4718ff0ee
Manuel Cota <56351434+mannynacc@users.noreply.github.com>
2022-06-23 23:43:29 -0600

    Separate get into functions

--- app/static/js/get_timeline_posts.js
+++ app/static/js/get_timeline_posts.js
@@ -6,17 +6,23 @@ async function getPosts () {
     return data;
 }
 
-posts = getPosts().then(data => console.log(data));
+function printPosts(posts) {
+    for(post in posts)
+    {
+        console.log(post);
+        $('#timeline').append(
+        `<div class="post" id="post-${post.id}>
+            <span class="name>${post.name}</span>
+            <span class="name>${post.email}</span>
+            <span class="name>${post.created_at}</span>
 
-for(post in posts)
-{
-    $('#timeline').append(
-    `<div class="post" id="post-${post.id}>
-        <span class="name>${post.name}</span>
-        <span class="name>${post.email}</span>
-        <span class="name>${post.created_at}</span>
-
-        <p>${post.content}</p>
-    </div>`
-    );
+            <p>${post.content}</p>
+        </div>`
+        );
+    }
 }
+
+posts = getPosts()
+        .then(data => console.log(data))
+        .then(posts => printPosts(posts));
+
