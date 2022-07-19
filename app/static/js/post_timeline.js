6784bcbb7d0450ee4807d17eac70112bc0a59b04
Manuel Cota <manuelcota01@outlook.com>
2022-07-19 03:42:05 +0000

    Show HTML when error occurs

--- app/static/js/post_timeline.js
+++ app/static/js/post_timeline.js
@@ -1,28 +1,44 @@
 const form = document.getElementById('form');
- 
-form.addEventListener('submit', function(e) {
-    // Prevent default behavior:
-    e.preventDefault();
+
+async function submit_post() {
     // Create payload as new FormData object:
     const payload = new FormData(form);
 
     // Post the payload using Fetch:
-    submit_post = await fetch('https://mlhportofolio.duckdns.org/api/timeline_post', {
+    post = await fetch('https://mlhportofolio.duckdns.org/api/timeline_post', {
     method: 'POST',
     body: payload,
     });
 
-    if (submit_post.status == 429) {
-        text = await res.text()
-
-        document.open();
-        document.write(text);
-        document.close();
+    if (!post.ok) {
+        if(post.status == 429) {
+            const body = document.body;
+            body.innerHTML = 
+            `
+                <center>
+                    <h1>429 Too Many Requests</h1>
+                    <p>Please wait 1 minute before trying to post again.</p>
+                </center>
+                <hr>
+                <center>nginx/1.23.0</center>
+            `;
+        }
+        else {
+            const error = (data && data.message) || post.status;
+            document.open();
+            document.write(error);
+            document.close();
+        }
     }
     else {
-
-        text = await res.text()
+        text = await post.text()
         console.log(text)
         location.reload()
     }
+}
+ 
+form.addEventListener('submit', function(e) {
+    // Prevent default behavior:
+    e.preventDefault();
+    submit_post();
 })
